d050b72d012249e0e346da437cf06525d6f545c8
githubawaq <97857484+perezbujeiro1980@users.noreply.github.com>
2023-04-05 18:07:49 -0300

    ok

---
+++ countCharacter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace countCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "Dinosaurio";
+            char[] stringChar = cadena.ToCharArray();
+            int total;
+            
+            for (int i = 0; i <= cadena.Length - 1; i++)
+            {
+              total = 0;
+              for (int j = 0; j <= cadena.Length - 1; j++)
+			{
+                if (cadena[i] == cadena[j]) {
+                    total = total + 1;                 
+                }
+			}
+
+            Console.WriteLine(cadena[i] + "- veces: " + total);
+              
+            }
+            Console.ReadLine();
+        }
+           
+    }
+
+}
